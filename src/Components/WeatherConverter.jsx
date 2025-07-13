1b0aec54a374b17da39f07ec35cb1238f8d4631e
sarojsubedi <sarojsubedi@proton.me>
2025-07-13 23:29:49 +0545

    added loading

--- src/Components/WeatherConverter.jsx
+++ src/Components/WeatherConverter.jsx
@@ -4,14 +4,17 @@ function WeatherConverter() {
     const [city, setCity] = useState("Kathmandu");
     const [utaKoResult, setUtaKoResult] = useState({});
     const [userInput, setUserInput] = useState("Kathmandu");
-    let error = false;
+    const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
 useEffect (()=>{
     const weather = async () => {
+        setIsLoading(true);
         const response = await fetch(`https://goweather.xyz/weather/${city}`);
         const result = await response.json();
         setUtaKoResult(result);
-        error = response.status == 404 ? true : false
+        setError(response.status == 404 ? true : false);
+        setIsLoading(false);
 };
 weather();
 }, [city]);
@@ -28,11 +31,14 @@ function storeCity(e){
             <input type="text" placeholder='Enter City Name' value = {userInput} onChange ={(e)=>setUserInput(e.target.value)}/>
             <button type="submit" onClick={(e)=> storeCity(e)}>Search</button>
         </form>
+
         <div>{error && <p>City not found</p>}</div>
-        <p>City: {city}</p>
+        {isLoading? (<p>Loading is a bit slow, have patience.🫩</p>):
+        (<div><p>City: {city}</p>
         <p>Temperature: {utaKoResult.temperature}</p>
         <p>Description : {utaKoResult.description}</p>
-        <p>Wind: {utaKoResult.wind}</p>
+        <p>Wind: {utaKoResult.wind}</p></div>)}
+        
         </>
     );
 }
